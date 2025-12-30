b706612d3f7908b49dd2836da6516a7d38d8c217
danishx7even <danish.7even@gmail.com>
2025-12-31 01:48:37 +0500

    Commented the set branch

--- wp-custom-dashboard.php
+++ wp-custom-dashboard.php
@@ -35,7 +35,7 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 );
 
 //Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('main');
+// $myUpdateChecker->setBranch('main');
 
 //Optional: If you're using a private repository, specify the access token like this:
 // $myUpdateChecker->setAuthentication('your-token-here');
